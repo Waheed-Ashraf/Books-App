99ecdd3f87917795e0ec0c5f520089288ceb29c4
waheed_ashrf <washraf124@gmail.com>
2024-01-25 01:53:52 +0200

    Dependency injection with get_it pachage

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,15 @@
+import 'package:books_app/Features/Home/data/repo/home_repo_implementation.dart';
+import 'package:books_app/core/utils/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+GetIt getIt = GetIt.instance;
+
+void serviceLocator() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoimp>(
+    HomeRepoimp(
+      getIt.get<ApiService>(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
-import 'package:books_app/Features/splash/presentation/View/splash_view.dart';
+import 'package:books_app/Features/Home/data/repo/home_repo_implementation.dart';
+import 'package:books_app/Features/Home/presentation/manager/NewestBooksCubit/newest_books_cubit.dart';
+import 'package:books_app/Features/Home/presentation/manager/featuerdBooksCubit/featuerd_books_cubit_cubit.dart';
 import 'package:books_app/constent.dart';
 import 'package:books_app/core/utils/app_router.dart';
+import 'package:books_app/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  serviceLocator();
   runApp(const BooksApp());
 }
 
@@ -13,13 +18,28 @@ class BooksApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeatuerdBooksCubitCubit(
+            getIt.get<HomeRepoimp>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            getIt.get<HomeRepoimp>(),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
